5f08469ccf065eb1caa43cc339884b6ee4d9b52e
Henriquelay <henriquelayber@gmail.com>
2021-03-12 02:47:13 -0300

    Aquilo n era UnionFind n, UnionFind é isso aqui crl

--- lib/unionFind.h
+++ lib/unionFind.h
@@ -1,15 +1,16 @@
 #ifndef _UNION_FIND_H_
 #define _UNION_FIND_H_
 
-#include <stdint.h>
 #include <stdlib.h>
 
-typedef uint8_t t_list;
+typedef struct union_t {
+    size_t* array;
+    size_t size;
+} union_t;
 
-t_list* UFinit(const size_t size);
-void UFdestroy(t_list* list);
-void UFmarkIndex(t_list* list, const size_t index);
-char UFisMarked(t_list* list, const size_t index);
-void UFprint(t_list* list, const size_t size);
+union_t* UF_init(const size_t size);
+union_t* UF_destroy(union_t* unionStruct);
+int UF_find(union_t* unionStruct, size_t index);
+void UF_union(union_t* unionStruct, const size_t p, const size_t q);
 
 #endif
--- src/unionFind.c
+++ src/unionFind.c
@@ -1,34 +1,35 @@
 #include "../lib/unionFind.h"
 
-#define VARSIZE sizeof(uint8_t)
-
-t_list* UFinit(const size_t size) {
-    t_list* list = (uint8_t*)calloc(size / VARSIZE, sizeof(uint8_t));
-    if (list == NULL) {
-        perror("Error allocating list");
+union_t* UF_init(const size_t size) {
+    union_t* newStruct = (union_t*)malloc(sizeof(union_t));
+    if (newStruct == NULL) {
+        perror("Error allocating unionFind struct. Exiting");
         exit(1);
     }
 
-    return list;
-}
+    newStruct->size = size;
+    newStruct->array = (size_t*)malloc(sizeof(size_t) * newStruct->size);
 
-void UFdestroy(t_list* list) {
-    free(list);
-    list = NULL;
-}
-
-void UFmarkIndex(t_list* list, const size_t index) {
-    list[index / VARSIZE] = list[index / VARSIZE] & (0x01 << index % VARSIZE);
-}
-
-char UFisMarked(t_list* list, const size_t index) {
-    return ((list[(index / VARSIZE)] >> index % VARSIZE) & 0x01);
-}
-
-void UFprint(t_list* list, const size_t size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", list[i / VARSIZE] & (0x01 << i % VARSIZE));
+    for (size_t i = 0;i < size;i++) {
+        newStruct->array[i] = i;
     }
-    puts("");
+
+    return newStruct;
 }
 
+union_t* UF_destroy(union_t* unionStruct) {
+    free(unionStruct->array);
+    free(unionStruct);
+    return NULL;
+}
+
+int UF_find(union_t* unionStruct, size_t index) {
+    while (unionStruct->array[index] != index) index = unionStruct->array[index];
+    return index;
+}
+
+void UF_union(union_t* unionStruct, const size_t p, const size_t q) {
+    size_t i = UF_find(unionStruct, p);
+    size_t j = UF_find(unionStruct, q);
+    unionStruct->array[i] = j;
+}
